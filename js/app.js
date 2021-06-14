2874906584547036d709d2c8fca60270864ff431
Spenser Spuck <spucksm@gmail.com>
2021-06-14 14:40:36 -0700

    started lab 6

---
+++ js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const storearray = [Seattle, Tokyo, Dubai, Paris, Lima];
+
+const seattle = {
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+}
+
+const Tokyo ={
+
+}
+
+const Dubai = {
+
+}
+
+const Paris = {
+
+}
+
+const Lima = {
+
+}
+
+function getRandom() {
+  return Math.random() * (max - min) + min;
+}
+
+
+let locationarray = [seattle]
+console.log(locationarray)
+// create a function to take in 2 numbers (min, max)
+// that generates a random number betwen min and max
+// east object will need a lsit (array), the ramdom number generator will be used to build list of random numbers for every hour on evry store (seattle today)
+
+
+// create global array for hours
